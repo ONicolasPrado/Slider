66c9f08708c2b7494df0d95e3f7ad1c28efc5a6b
Nícolas do Prado <nicolasprado.ndp@gmail.com>
2024-08-14 13:11:28 -0300

    change css

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-let btnNext = document.querySelector('.next')
-let btnBack = document.querySelector('.back')
+let btnNext = document.querySelector(".next");
+let btnBack = document.querySelector(".back");
 
-let container = document.querySelector('.container')
-let list = document.querySelector('.container .list')
-let thumb = document.querySelector('.container .thumb')
+let container = document.querySelector(".container");
+let list = document.querySelector(".container .list");
+let thumb = document.querySelector(".container .thumb");
 
-btnNext.onclick = () => moveItemsOnClick('next')
-btnBack.onclick = () => moveItemsOnClick('back')
+btnNext.onclick = () => moveItemsOnClick("next");
+btnBack.onclick = () => moveItemsOnClick("back");
 
 function moveItemsOnClick(type) {
-    let listItems = document.querySelectorAll('.list .list-item')
-    let thumbItems = document.querySelectorAll('.thumb .thumb-item')
+  let listItems = document.querySelectorAll(".list .list-item");
+  let thumbItems = document.querySelectorAll(".thumb .thumb-item");
 
-    console.log(listItems)
-    console.log(thumbItems)
+  if (type === "next") {
+    list.appendChild(listItems[0]);
+    thumb.appendChild(thumbItems[0]);
+    container.classList.add("next");
+  } else {
+    list.prepend(listItems[listItems.length - 1]);
+    thumb.prepend(thumbItems[listItems.length - 1]);
+    container.classList.add("back");
+  }
 
-    if(type === 'next'){
-        list.appendChild(listItems[0])
-        thumb.appendChild(thumbItems[0])
-        container.classList.add('next')
-    } else{
-        list.prepend(listItems[listItems.length - 1])
-        thumb.prepend(thumbItems[thumbItems.length - 1])
-        container.classList.add('back')
-    }
-
-    setTimeout(() => {
-        container.classList.remove('next')
-        container.classList.remove('back')
-    }, 1000);
+  setTimeout( () => {
+    container.classList.remove("next");
+    container.classList.remove("back");
+  }, 1000);
 }
